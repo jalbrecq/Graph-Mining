3df9c6ddeaceeaee52c23a5cea30dd506e6eb636
jalbrecq <jeanalbrecq@gmail.com>
2019-10-27 17:18:38 +0100

    generate random networks

--- louvain_method.py
+++ louvain_method.py
@@ -2,22 +2,35 @@ import community
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.karate_club_graph()
+data = []
 
-# compute the best partition
-partition = community.best_partition(G)
+for graph_id in range(0, 102):
 
-# draw the graph
-size = float(len(set(partition.values())))
-pos = nx.spring_layout(G)
-count = 0.
-for com in set(partition.values()):
-    count = count + 1.
-    list_nodes = [nodes for nodes in partition.keys()
-                  if partition[nodes] == com]
-    # nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=str(count * 1. / size))
-    nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=(count * 0.99/ size,count * 0.99/ size, count * 0.01 / size))
+    if(graph_id == 0):
+        G = nx.karate_club_graph()
+    else:
+        G = nx.gnm_random_graph(34, 78)
+    
+    data.append([])
 
-nx.draw_networkx_edges(G, pos, alpha=0.2)
+    # compute the best partition
+    partition = community.best_partition(G)
 
-plt.show()
+    # draw the graph
+    size = float(len(set(partition.values())))
+    pos = nx.spring_layout(G)
+    count = 0.
+
+    for i, com in enumerate(set(partition.values())):
+
+        count = count + 1.
+        list_nodes = [nodes for nodes in partition.keys()
+                      if partition[nodes] == com]
+        # nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=str(count * 1. / size))
+        nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=(count * 0.99/ size,count * 0.99/ size, count * 0.01 / size))
+
+        print(data)
+
+        data[graph_id].append((i, len(list_nodes)))
+
+    nx.draw_networkx_edges(G, pos, alpha=0.2)

ff38cc4f2e20d4c68d95fc8067ecdd2c507335b8
jalbrecq <32439885+jalbrecq@users.noreply.github.com>
2019-10-28 12:00:48 +0100

    update display

--- 5-louvain_method.py
+++ 5-louvain_method.py
@@ -10,7 +10,7 @@ for graph_id in range(0, 102):
         G = nx.karate_club_graph()
     else:
         G = nx.gnm_random_graph(34, 78)
-    
+
     data.append([])
 
     # compute the best partition
@@ -18,19 +18,17 @@ for graph_id in range(0, 102):
 
     # draw the graph
     size = float(len(set(partition.values())))
-    pos = nx.spring_layout(G)
-    count = 0.
 
     for i, com in enumerate(set(partition.values())):
 
-        count = count + 1.
         list_nodes = [nodes for nodes in partition.keys()
                       if partition[nodes] == com]
-        # nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=str(count * 1. / size))
-        nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=(count * 0.99/ size,count * 0.99/ size, count * 0.01 / size))
-
-        print(data)
 
         data[graph_id].append((i, len(list_nodes)))
 
-    nx.draw_networkx_edges(G, pos, alpha=0.2)
+    pos = nx.spring_layout(G)
+    plt.figure(figsize=(8, 8))
+    plt.axis('off')
+    nx.draw_networkx_nodes(G, pos, node_size=600, cmap=plt.cm.RdYlBu, node_color=list(partition.values()))
+    nx.draw_networkx_edges(G, pos, alpha=0.3)
+    plt.show(G)

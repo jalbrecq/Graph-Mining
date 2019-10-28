30236cee93009594979bb10a41c90deeaffa9960
jalbrecq <32439885+jalbrecq@users.noreply.github.com>
2019-10-28 12:00:33 +0100

    testing display

--- test.py
+++ test.py
@@ -1,25 +1,14 @@
-import community
 import networkx as nx
+import community
 import matplotlib.pyplot as plt
 
-G = nx.gnm_random_graph(34, 78)
+G = nx.karate_club_graph()  # load a default graph
 
-# compute the best partition
-partition = community.best_partition(G)
+partition = community.best_partition(G)  # compute communities
 
-# draw the graph
-size = float(len(set(partition.values())))
-pos = nx.spring_layout(G)
-count = 0.
-
-for i, com in enumerate(set(partition.values())):
-
-    count = count + 1.
-    list_nodes = [nodes for nodes in partition.keys()
-                      if partition[nodes] == com]
-    # nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=str(count * 1. / size))
-    nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20, node_color=(count * 0.99/ size,count * 0.99/ size, count * 0.01 / size))
-
-    nx.draw_networkx_edges(G, pos, alpha=0.2)
-
-plt.show()
+pos = nx.spring_layout(G)  # compute graph layout
+plt.figure(figsize=(8, 8))  # image is 8 x 8 inches
+plt.axis('off')
+nx.draw_networkx_nodes(G, pos, node_size=600, cmap=plt.cm.RdYlBu, node_color=list(partition.values()))
+nx.draw_networkx_edges(G, pos, alpha=0.3)
+plt.show(G)

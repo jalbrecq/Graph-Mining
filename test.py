3bb46fd4c55382e827f9f6de8a16cb0d9e8fcf6c
jalbrecq <jeanalbrecq@gmail.com>
2019-10-28 17:11:57 +0100

    testing encours

--- test.py
+++ test.py
@@ -1,14 +1,50 @@
-import networkx as nx
-import community
-import matplotlib.pyplot as plt
+import random as rdm
 
-G = nx.karate_club_graph()  # load a default graph
+#Reads the graph from a GML File, named lesmis.gml
+G = nx.read_gml("lesmis.gml",relabel=True)
 
-partition = community.best_partition(G)  # compute communities
+#Start the counter
+counter = 0
 
-pos = nx.spring_layout(G)  # compute graph layout
-plt.figure(figsize=(8, 8))  # image is 8 x 8 inches
-plt.axis('off')
-nx.draw_networkx_nodes(G, pos, node_size=600, cmap=plt.cm.RdYlBu, node_color=list(partition.values()))
-nx.draw_networkx_edges(G, pos, alpha=0.3)
-plt.show(G)
+#Start execution counter
+execution = 0
+
+#Execute 10 times this command sequence
+while execution < 10:
+    #Choose a random start node
+    vertexid = rdm.choice(G.nodes())
+    #Dictionary that associate nodes with the amount of times it was visited
+    VisitedVertex = {}
+
+    print("Execucao n°" % (execution + 1))
+    #Execute the random walk with size 10000 (10000 steps)
+    while counter < 10000: 
+        #Accumulate the amount of times each vertex is visited
+        if vertexid in VisitedVertex:
+                VisitedVertex[vertexid] += 1
+        else:
+                VisitedVertex[vertexid] = 1
+        #Visualize the vertex neighborhood
+        Vertex_Neighbors = G.neighbors(vertexid)
+        #Choose a vertex from the vertex neighborhood to start the next random walk
+        vertexid = rdm.choice(Vertex_Neighbors)
+        #Iteration counter increment
+        counter = counter + 1
+
+    #Organize the vertex list in most visited decrescent order
+    mostvisited = sorted(VisitedVertex, key = VisitedVertex.get,reverse = True)
+    #Separate the top 10 most visited vertex
+    top_10 = mostvisited[:10]
+    print(top_10)
+
+    #Restart the cycle
+    execution = execution + 1
+    counter = 0
+
+print("Feche a janela de exibicao para encerrar.")
+
+#Draw graph to a window
+nx.draw_circular(G)
+
+plt.savefig('myfig')
+plt.show()

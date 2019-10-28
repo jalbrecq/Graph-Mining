740b1873fed31287bc482d62f152f063c8a10fc1
jalbrecq <jeanalbrecq@gmail.com>
2019-10-28 17:46:24 +0100

    Display the graph

--- 7-random_walker.py
+++ 7-random_walker.py
@@ -1,13 +1,14 @@
 import networkx as nx
 import random as rdm
+import matplotlib.pyplot as plt
 
 # generate the graph
 G = nx.karate_club_graph()
 
 # choose a random starting possiton
 possition = rdm.choice(list(G.nodes))
-print(possition)
-print(G.degree[possition])
+print("Starting node =", possition)
+print("Degree of the starting node =", G.degree[possition])
 
 visited_nodes = {}
 
@@ -24,10 +25,20 @@ while len(visited_nodes) != 34:
     possition = rdm.choice(neighbors)
 
     step+=1
-
     pass
 
-mostvisited = sorted(visited_nodes, key = visited_nodes.get,reverse = True)
-print(step)
-print(visited_nodes)
-print(mostvisited)
+nodes, y = zip(*visited_nodes.items())
+fig, ax = plt.subplots()
+plt.bar(nodes, y, width=0.9, color=(0, 0.5, 0.7, 1))
+plt.title("Histogramme du nombre de passage")
+plt.ylabel("Count")
+plt.xlabel("Nodes")
+ax.set_xticks([x for x in nodes])
+ax.set_xticklabels(nodes)
+
+print("Number of step =", step)
+# mostvisited = sorted(visited_nodes, key = visited_nodes.get,reverse = True)
+# print(visited_nodes)
+# print(mostvisited)
+
+plt.show()

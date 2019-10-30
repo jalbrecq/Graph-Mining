0317989745a61149812ce9d444bd197bb3b6b4e4
jalbrecq <32439885+jalbrecq@users.noreply.github.com>
2019-10-30 20:13:46 +0100

    fin du monsieur qui marche

--- 7-random_walker.py
+++ 7-random_walker.py
@@ -2,79 +2,76 @@ import networkx as nx
 import random as rdm
 import matplotlib.pyplot as plt
 
+def create_graph(dic, titre, y_label, x_label):
+    nodes, y = zip(*dic.items())
+    fig, ax = plt.subplots()
+    plt.bar(nodes, y, width=0.9, color=(0, 0.5, 0.7, 1))
+    plt.title(titre)
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    ax.set_xticks([x for x in nodes])
+    ax.set_xticklabels(nodes)
+    plt.show()
+
 # generate the graph
 G = nx.karate_club_graph()
 
-data = {}
-data2 = {}
+step_by_start = {}
+step_by_start_degree = {}
 
-for x in range(0, 1000):
-	for start in range(0, 34):
+for x in range(0, 10000):
+    av_visited_nodes = {}
+    print("ITERATION : ", x)
 
-		print("Starting node =", start)
-		print("Degree of the starting node =", G.degree[start])
+    for start in range(0, 34):
+        step = 0
+        possition = start
 
-		visited_nodes = {} 
+        visited_nodes = {}
+        while len(visited_nodes) != 34:
 
-		step = 0
-		possition = start
+            if possition in visited_nodes:
+                visited_nodes[possition]+=1
+            else:
+                visited_nodes[possition]=1
 
-		while len(visited_nodes) != 34:
+            neighbors = list(G.neighbors(possition))
+            possition = rdm.choice(neighbors)
 
-		    if possition in visited_nodes:
-		        visited_nodes[possition]+=1
-		    else:
-		        visited_nodes[possition]=1
+            step+=1
+            pass
 
-		    neighbors = list(G.neighbors(possition))
-		    possition = rdm.choice(neighbors)
+        if start in step_by_start:
+            step_by_start[start] = (step_by_start[start] + step) / 2
+        else:
+            step_by_start[start] = step
 
-		    step+=1
-		    pass
+        if G.degree[start] in step_by_start_degree:
+            step_by_start_degree[G.degree[start]] = (step_by_start_degree[G.degree[start]] + step) / 2
+        else:
+            step_by_start_degree[G.degree[start]] = step
 
-		if start in data2:
-			old_step = data2[start]
-			data2[start] = (old_step + step)/2
-		else:
-			data2[start] = step
+        for node in visited_nodes:
+            if node in av_visited_nodes:
+                av_visited_nodes[node] = (av_visited_nodes[node] + visited_nodes[node]) / 2
+            else:
+                av_visited_nodes[node] = visited_nodes[node]
 
-		if G.degree[start] in data:
-			old_step = data[G.degree[start]]
-			data[G.degree[start]] = (old_step + step)/2
-		else:
-			data[G.degree[start]] = step
+# number of step by starting node
+create_graph(step_by_start, "Nombre de pas en fonction de la node de depart", "Steps", "Starting Nodes")
 
-		# nodes, y = zip(*visited_nodes.items())
-		# fig, ax = plt.subplots()
-		# plt.bar(nodes, y, width=0.9, color=(0, 0.5, 0.7, 1))
-		# plt.title("Histogramme du nombre de passage")
-		# plt.ylabel("Count")
-		# plt.xlabel("Nodes")
-		# ax.set_xticks([x for x in nodes])
-		# ax.set_xticklabels(nodes)
+# number of step by degree of the starting node
+create_graph(step_by_start_degree, "Nombre de pas en fonction du degre de la node de depart", "Steps", "Degree of starting Nodes")
 
-		print("Number of step =", step)
-		print("-"*40)
-		# mostvisited = sorted(visited_nodes, key = visited_nodes.get,reverse = True)
-		# print(visited_nodes)
-		# print(mostvisited)
+# number of step by node
+create_graph(av_visited_nodes, "Nombre de passages en fonction de la node", "Count", "Nodes")
 
-degrees, steps = zip(*data.items())
-fig, ax = plt.subplots()
-plt.bar(degrees, steps, width=0.9, color=(0, 0.5, 0.7, 1))
-plt.title("Nombre de passage en fonction du degre de la node de depart")
-plt.ylabel("Steps")
-plt.xlabel("Degree")
-ax.set_xticks([x for x in degrees])
-ax.set_xticklabels(degrees)
+# number of step by degree of nodes
+visited_nodes_by_degree = {}
+for node in av_visited_nodes:
+    if G.degree[node] in visited_nodes_by_degree:
+        visited_nodes_by_degree[G.degree[node]] = (visited_nodes_by_degree[G.degree[node]] + av_visited_nodes[node]) / 2
+    else:
+        visited_nodes_by_degree[G.degree[node]] = av_visited_nodes[node]
+create_graph(visited_nodes_by_degree, "Nombre de passages en fonction du degre de la node", "Count", "Degree")
 
-nodes, steps = zip(*data2.items())
-fig, ax = plt.subplots()
-plt.bar(nodes, steps, width=0.9, color=(0, 0.5, 0.7, 1))
-plt.title("Nombre de passage en fonction de la node de depart")
-plt.ylabel("Steps")
-plt.xlabel("Nodes")
-ax.set_xticks([x for x in nodes])
-ax.set_xticklabels(nodes)
-
-plt.show()

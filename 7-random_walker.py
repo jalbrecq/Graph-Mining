243d8ed0deba9aef5e346da74ec1acc960ce218b
jalbrecq <jeanalbrecq@gmail.com>
2019-11-04 10:51:27 +0100

    rename graphs

--- 7-random_walker.py
+++ 7-random_walker.py
@@ -70,7 +70,7 @@ create_graph(step_by_start, "Nombre de pas en fonction de la node de depart", "S
 create_graph(step_by_start_degree, "Nombre de pas en fonction du degre de la node de depart", "Steps", "Degree of starting Nodes")
 
 # number of step by node
-create_graph(av_visited_nodes, "Nombre de passages en fonction de la node", "Count", "Nodes")
+create_graph(av_visited_nodes, "Nombre de passages dans une node", "Count", "Nodes")
 
 # number of step by degree of nodes
 visited_nodes_by_degree = {}
@@ -79,7 +79,7 @@ for node in av_visited_nodes:
         visited_nodes_by_degree[G.degree[node]] = (visited_nodes_by_degree[G.degree[node]] + av_visited_nodes[node]) / 2
     else:
         visited_nodes_by_degree[G.degree[node]] = av_visited_nodes[node]
-create_graph(visited_nodes_by_degree, "Nombre de passages en fonction du degre de la node", "Count", "Degree")
+create_graph(visited_nodes_by_degree, "Nombre de passages dans une node en fonction de son degre", "Count", "Degree")
 
 degree, steps = zip(*box_plot_data.items())
 plt.boxplot(list(steps), patch_artist=True, labels=list(degree))
